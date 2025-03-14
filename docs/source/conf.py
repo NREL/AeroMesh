6b9453c48be9651b70eabca47c243a55211acc04
agushin101 <agushin101@gmail.com>
2025-03-14 14:21:07 -0600

    add theme to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,11 +7,14 @@ copyright = '2024, National Renewable Energy Laboratory'
 author = 'National Renewable Energy Laboratory'
 release = 'v0.1'
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
